39af048d0ea22c6a80851627a4281efeabc9820a
Iyxan23 <nurihsanalghifari@gmail.com>
2020-12-13 16:13:54 +0700

    Some little changes

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// This demo is using a fixed data, in release, you would probably need to use a backend to dynamically make quizzes
+var data = [
+    {
+        "question": "Question 1",
+        "options": [
+            "A. option A",
+            "B. option B",
+            "C. option C",
+            "D. option D"
+        ]
+    },
+    {
+        "question": "Question 2",
+        "options": [
+            "A. option A",
+            "B. option B",
+            "C. option C",
+            "D. option D"
+        ]
+    },
+    {
+        "question": "Question 3",
+        "options": [
+            "A. option A",
+            "B. option B",
+            "C. option C",
+            "D. option D"
+        ]
+    },
+    {
+        "question": "Question 4",
+        "options": [
+            "A. option A",
+            "B. option B",
+            "C. option C",
+            "D. option D"
+        ]
+    },
+    {
+        "question": "Question 5",
+        "options": [
+            "A. option A",
+            "B. option B",
+            "C. option C",
+            "D. option D"
+        ]
+    }
+];
+var question_num = 0;
