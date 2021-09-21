ff52b510f4c0703c8c4530e07ef6f45aad3b2559
Iyxan23 <nurihsanalghifari@gmail.com>
2021-09-21 11:49:18 +0700

    feat: moved hover codes over to js and use JetBrains Mono

--- js/script.js
+++ js/script.js
@@ -46,4 +46,48 @@ var data = [
         ]
     }
 ];
-var question_num = 0;
+
+var selection = -1;
+let options = document.getElementById("options");
+let children = options.children;
+
+for (let i = 0; i < children.length; i++) {
+    var element = children[i].children[0];
+    console.log(element);
+
+    element.addEventListener("mouseover", (e) => {
+        enable_option(e.target);
+    });
+
+    element.addEventListener("mouseout", (e) => {
+        if (selection != i) {
+            disable_option(e.target);
+        }
+    });
+
+    element.addEventListener("click", (e) => {
+        if (selection == i) {
+            disable_option(e.target);
+            selection = -1;
+
+            return;
+        }
+
+        if (selection != -1) {
+            disable_option(children[selection].children[0]);
+        }
+
+        selection = i;
+        enable_option(children[selection].children[0]);
+    });
+}
+
+function enable_option(option) {
+    option.style.color = "#171717";
+    option.style.backgroundColor = "#FFFFFF";
+}
+
+function disable_option(option) {
+    option.style.color = "#FFFFFF";
+    option.style.backgroundColor = "#171717";
+}
